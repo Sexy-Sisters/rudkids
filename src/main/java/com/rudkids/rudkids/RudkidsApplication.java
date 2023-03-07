c7b9496d2dac20ac019b9faeb4e11943574f5653
nswon <swnam0729@gmail.com>
2023-03-07 20:14:02 +0900

    Initial commit

---
+++ src/main/java/com/rudkids/rudkids/RudkidsApplication.java
@@ -0,0 +1,13 @@
+package com.rudkids.rudkids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RudkidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RudkidsApplication.class, args);
+	}
+
+}
