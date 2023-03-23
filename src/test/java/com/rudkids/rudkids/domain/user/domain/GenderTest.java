0ddf6d8d63550d9330e81aa1a18a7d2a9e2cee8a
nswon <swnam0729@gmail.com>
2023-03-23 20:50:56 +0900

    feat: add domain and service test

---
+++ src/test/java/com/rudkids/rudkids/domain/user/domain/GenderTest.java
@@ -0,0 +1,23 @@
+package com.rudkids.rudkids.domain.user.domain;
+
+import com.rudkids.rudkids.domain.user.exception.InvalidGenderException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class GenderTest {
+
+    @DisplayName("성별 양식에 맞지 않을 시 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"MALEL", "FEMAILL", " "})
+    @NullSource
+    @EmptySource
+    void create_Exception_Format(String invalid) {
+        assertThatThrownBy(() -> Gender.toEnum(invalid))
+                .isInstanceOf(InvalidGenderException.class);
+    }
+}
