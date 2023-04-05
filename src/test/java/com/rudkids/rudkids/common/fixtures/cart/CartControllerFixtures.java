17e2206e96f021a7b7a4a163f937e9f4f9499a42
nswon <swnam0729@gmail.com>
2023-04-05 15:08:57 +0900

    feat: 인수 테스트 작성 (#58)

--- src/test/java/com/rudkids/rudkids/common/fixtures/cart/CartControllerFixtures.java
+++ src/test/java/com/rudkids/rudkids/common/fixtures/cart/CartControllerFixtures.java
@@ -1,7 +1,12 @@
 package com.rudkids.rudkids.common.fixtures.cart;
 
+import com.rudkids.rudkids.domain.cart.application.CartInfo;
+import com.rudkids.rudkids.domain.cart.application.CartItemInfo;
+import com.rudkids.rudkids.domain.item.domain.ItemStatus;
+import com.rudkids.rudkids.domain.item.domain.LimitType;
 import com.rudkids.rudkids.interfaces.cart.dto.CartRequest;
 
+import java.util.List;
 import java.util.UUID;
 
 public class CartControllerFixtures {
@@ -9,8 +14,18 @@ public class CartControllerFixtures {
     public static final String CART_DEFAULT_URL = "/api/v1/cart";
     public static final UUID 아이템_ID = UUID.randomUUID();
     public static final int 아이템_수량 = 2;
+    public static final int 장바구니_아이템_총_가격 = 9980;
+    public static final List<CartItemInfo.Main> 장바구니_아이템 = List.of(
+            new CartItemInfo.Main("옷", 1000, ItemStatus.IN_STOCK),
+            new CartItemInfo.Main("스티커", 500, ItemStatus.SOLD_OUT),
+            new CartItemInfo.Main("알약", 990, ItemStatus.IN_STOCK)
+    );
 
     public static CartRequest.AddCartItem CART_아이템_추가_요청() {
         return new CartRequest.AddCartItem(아이템_ID, 아이템_수량);
+    }
+
+    public static CartInfo.Main CART_아이템_리스트() {
+        return new CartInfo.Main(장바구니_아이템_총_가격, 장바구니_아이템);
     }
 }
--- src/test/java/com/rudkids/rudkids/interfaces/cart/CartControllerTest.java
+++ src/test/java/com/rudkids/rudkids/interfaces/cart/CartControllerTest.java
@@ -6,19 +6,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
-import static com.rudkids.rudkids.common.fixtures.cart.CartControllerFixtures.CART_DEFAULT_URL;
-import static com.rudkids.rudkids.common.fixtures.cart.CartControllerFixtures.CART_아이템_추가_요청;
+import static com.rudkids.rudkids.common.fixtures.cart.CartControllerFixtures.*;
 import static com.rudkids.rudkids.common.fixtures.user.UserControllerFixtures.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -52,6 +52,43 @@ class CartControllerTest extends ControllerTest {
                                 fieldWithPath("amount")
                                         .type(JsonFieldType.NUMBER)
                                         .description("아이템 수량")
+                        )
+                ))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("장바구니에 담겨있는 아이템 리스트를 조회한다.")
+    @Test
+    void 장바구니에_담겨있는_아이템_리스트를_조회한다() throws Exception {
+        given(cartService.findCartItems(any()))
+                .willReturn(CART_아이템_리스트());
+
+        mockMvc.perform(get(CART_DEFAULT_URL)
+                        .header(AUTHORIZATION_HEADER_NAME, AUTHORIZATION_HEADER_VALUE))
+                .andDo(print())
+                .andDo(document("cart/findCartItems",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization")
+                                        .description("JWT Access Token")
+                        ),
+                        responseFields(
+                                fieldWithPath("totalCartItemPrice")
+                                        .type(JsonFieldType.NUMBER)
+                                        .description("장바구니아이템 총 가격"),
+
+                                fieldWithPath("cartItems.[].name")
+                                        .type(JsonFieldType.STRING)
+                                        .description("장바구니아이템 이름"),
+
+                                fieldWithPath("cartItems.[].price")
+                                        .type(JsonFieldType.NUMBER)
+                                        .description("장바구니아이템 가격"),
+
+                                fieldWithPath("cartItems.[].itemStatus")
+                                        .type(JsonFieldType.STRING)
+                                        .description("장바구니아이템 상태")
                         )
                 ))
                 .andExpect(status().isOk());
