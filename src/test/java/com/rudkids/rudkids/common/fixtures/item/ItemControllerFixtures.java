5b8a3950459a5bf98cbe68bac023f45093b96092
leekyukin <leekujin14@gmail.com>
2023-04-05 12:22:46 +0900

    style: 클레스명 변경

--- src/test/java/com/rudkids/rudkids/common/fixtures/item/ItemControllerFixtures.java
+++ src/test/java/com/rudkids/rudkids/common/fixtures/item/ItemControllerFixtures.java
@@ -3,13 +3,13 @@ package com.rudkids.rudkids.common.fixtures.item;
 import com.github.f4b6a3.ulid.UlidCreator;
 import com.rudkids.rudkids.domain.item.ItemCommand;
 import com.rudkids.rudkids.domain.item.ItemInfo;
-import com.rudkids.rudkids.domain.item.domain.item.ItemStatus;
-import com.rudkids.rudkids.domain.item.domain.item.LimitType;
+import com.rudkids.rudkids.domain.item.domain.ItemStatus;
+import com.rudkids.rudkids.domain.item.domain.LimitType;
 
 import java.util.List;
 import java.util.UUID;
 
-public class ItemControllerFixture {
+public class ItemControllerFixtures {
 
     public static final String ITEM_DEFAULT_URL = "/api/v1/item";
     public static final UUID 프로덕트_아이디 = UlidCreator.getMonotonicUlid().toUuid();
@@ -23,8 +23,8 @@ public class ItemControllerFixture {
     public static final String 아이템_상태_판매중 = ItemStatus.ON_SALES.name();
     public static final String 아이템_상태_솔드아웃 = ItemStatus.END_OF_SALES.name();
 
-    public static ItemCommand.RegisterRequest ITEM_등록_요청() {
-        return ItemCommand.RegisterRequest.builder()
+    public static ItemCommand.RegisterItemRequest ITEM_등록_요청() {
+        return ItemCommand.RegisterItemRequest.builder()
             .productId(프로덕트_아이디)
             .name(아이템_이름)
             .price(아이템_가격)
