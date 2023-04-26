ec0b642583bc28563968eeed0d454eb0da64651b
leekyukin <leekujin14@gmail.com>
2023-04-26 16:55:52 +0900

    style: 폴더 이동

--- src/main/java/com/rudkids/rudkids/domain/product/service/strategy/productStatus/OpenStrategy.java
+++ src/main/java/com/rudkids/rudkids/domain/product/service/strategy/productStatus/OpenStrategy.java
@@ -1,18 +1,18 @@
-package com.rudkids.rudkids.domain.product.service.strategy;
+package com.rudkids.rudkids.domain.product.service.strategy.productStatus;
 
 import com.rudkids.rudkids.domain.product.domain.Product;
 import com.rudkids.rudkids.domain.product.domain.ProductStatus;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CloseStrategy implements ChangeProductStatusStrategy {
+public class OpenStrategy implements ChangeProductStatusStrategy {
     @Override
     public boolean isProductStatus(ProductStatus productStatus) {
-        return ProductStatus.CLOSED == productStatus;
+        return ProductStatus.OPEN == productStatus;
     }
 
     @Override
     public void changeStatus(Product product) {
-        product.close();
+        product.open();
     }
 }
