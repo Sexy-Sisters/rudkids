c7ca79a38b2445ca1ac3a0c08d5288e3017517b5
nswon <swnam0729@gmail.com>
2023-05-18 12:28:23 +0900

    fix: 리뷰 수정

--- src/main/java/com/rudkids/rudkids/global/config/properties/TossPaymentProperties.java
+++ src/main/java/com/rudkids/rudkids/global/config/properties/TossPaymentProperties.java
@@ -1,20 +1,14 @@
 package com.rudkids.rudkids.global.config.properties;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("payment.toss")
+@RequiredArgsConstructor
 @Getter
 public class TossPaymentProperties {
     private final String clientKey;
     private final String secretKey;
     private final String confirmUri;
-
-    public TossPaymentProperties(String clientKey,
-                                 String secretKey,
-                                 String confirmUri) {
-        this.clientKey = clientKey;
-        this.secretKey = secretKey;
-        this.confirmUri = confirmUri;
-    }
 }
