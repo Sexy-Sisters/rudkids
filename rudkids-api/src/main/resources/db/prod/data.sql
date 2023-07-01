939fc6802e7a1c6998c75ee7633b9a50a979c22b
nswon <swnam0729@gmail.com>
2023-07-01 23:04:41 +0900

    fix: alter command 제거

--- rudkids-api/src/main/resources/db/prod/data.sql
+++ rudkids-api/src/main/resources/db/prod/data.sql
@@ -2,8 +2,3 @@ insert into tbl_user(user_id, email, name, phone_number, deleted, path, url, rol
 select X'0dda3b177a0c4c6888724af6f5425035', 'xorud6761@gmail.com', '김태경', '01012345678', false, 'path', 'http://k.kakaocdn.net/dn/bPPLA2/btshAzroPsi/aheqqZR70QtzADLKLWk0v1/img_640x640.jpg', 'ADMIN'
 from dual
 where not exists (select * from tbl_user where user_id = X'0dda3b177a0c4c6888724af6f5425035');
-
-ALTER TABLE tbl_user
-DROP COLUMN age,
-DROP COLUMN gender,
-DROP COLUMN social_type;
