feefe616c8b9b7b1ae1987883296b9b3d30c525b
nswon <swnam0729@gmail.com>
2023-07-05 10:42:25 +0900

    feat: image delete method 추가

--- rudkids-core/src/main/java/com/rudkids/core/user/exception/PhoneNumberEmptyException.java
+++ rudkids-core/src/main/java/com/rudkids/core/user/exception/PhoneNumberEmptyException.java
@@ -1,8 +1,8 @@
 package com.rudkids.core.user.exception;
 
-import com.rudkids.core.common.exception.BadRequestException;
+import com.rudkids.core.common.exception.NotFoundException;
 
-public class PhoneNumberEmptyException extends BadRequestException {
+public class PhoneNumberEmptyException extends NotFoundException {
     private static final String MESSAGE = "전화번호가 비어있습니다.";
 
     public PhoneNumberEmptyException() {
