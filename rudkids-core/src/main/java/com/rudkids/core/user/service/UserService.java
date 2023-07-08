515f7c4877e70ca220c3f6f9d5b6f80f08876e0c
nswon <swnam0729@gmail.com>
2023-07-08 12:30:48 +0900

    feat: 자기 자신이 쓴 커뮤니티 글들 조회

--- rudkids-core/src/main/java/com/rudkids/core/user/service/UserService.java
+++ rudkids-core/src/main/java/com/rudkids/core/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.rudkids.core.user.service;
 
+import com.rudkids.core.community.dto.CommunityResponse;
 import com.rudkids.core.user.dto.UserRequest;
 import com.rudkids.core.user.dto.UserResponse;
 
@@ -9,5 +10,6 @@ import java.util.UUID;
 public interface UserService {
     void update(UUID userId, UserRequest.Update request);
     UserResponse.Info get(UUID userId);
+    List<CommunityResponse.Main> getMyCommunities(UUID userId);
     List<UserResponse.Address> getAddresses(UUID userId);
 }
