05a978d44ecdb1d195084f4b717e32df284ce762
nswon <swnam0729@gmail.com>
2023-07-16 04:39:00 +0900

    fix: feign 변경

--- rudkids-core/src/main/java/com/rudkids/core/payment/infrastructure/TossPaymentCancelClient.java
+++ rudkids-core/src/main/java/com/rudkids/core/payment/infrastructure/TossPaymentCancelClient.java
@@ -1,20 +1,18 @@
 package com.rudkids.core.payment.infrastructure;
 
+import com.rudkids.core.config.FeignHeaderConfig;
 import com.rudkids.core.payment.dto.TossPaymentRequest;
 import feign.FeignException;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "TossPaymentCancelClient", url = "https://api.tosspayments.com/v1/payments/")
+@FeignClient(name = "TossPaymentCancelClient", url = "https://api.tosspayments.com/v1/payments/", configuration = {FeignHeaderConfig.class})
 public interface TossPaymentCancelClient {
 
     @PostMapping("/{paymentKey}/cancel")
     void cancel(
-        @RequestHeader HttpHeaders headers,
         @PathVariable("paymentKey") String paymentKey,
         @RequestBody TossPaymentRequest.Cancel request
     ) throws FeignException;
