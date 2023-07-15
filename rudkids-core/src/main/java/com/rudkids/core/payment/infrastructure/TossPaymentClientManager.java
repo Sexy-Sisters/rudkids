4631fb7ebf7ca49d385893ff2da29bcaeed44fbe
nswon <swnam0729@gmail.com>
2023-07-15 23:21:47 +0900

    fix: header 변경

--- rudkids-core/src/main/java/com/rudkids/core/payment/infrastructure/TossPaymentClientManager.java
+++ rudkids-core/src/main/java/com/rudkids/core/payment/infrastructure/TossPaymentClientManager.java
@@ -62,7 +62,8 @@ public class TossPaymentClientManager implements PaymentClientManager {
     private HttpHeaders generateHeader() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBasicAuth(properties.getSecretKey());
+//        headers.setBasicAuth(properties.getSecretKey());
+        headers.set("Authorization", "Basic " + properties.getSecretKey());
         return headers;
     }
 }
