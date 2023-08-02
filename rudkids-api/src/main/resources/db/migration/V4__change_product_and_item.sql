e615daa551c2fc393e6fe2be33c1f9eebb3703a3
nswon <swnam0729@gmail.com>
2023-08-02 19:31:52 +0900

    fix: v4 수정

--- rudkids-api/src/main/resources/db/migration/V4__change_product_and_item.sql
+++ rudkids-api/src/main/resources/db/migration/V4__change_product_and_item.sql
@@ -1,12 +1,4 @@
-alter table tbl_product change column mobile_path mobile_banner_path varchar(255) null;
-alter table tbl_product change column mobile_url mobile_banner_url varchar(255) null;
-
-alter table tbl_product drop column mystery;
-alter table tbl_product drop column product_status;
-
-alter table tbl_item drop column mystery;
-
-alter table tbl_item add column mystery_product_id binary(16) null;
-
+alter table tbl_item
+    add column mystery_product_id binary(16) null,
 add constraint FKtju2cxa4jdl3g2jbp61wuhy4m
         foreign key (mystery_product_id) references tbl_mystery_product (mystery_product_id);
